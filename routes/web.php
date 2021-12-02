fac7b49b338d83e50b50dc65522ca0ecbcd7af0b
Toshio Abe <abtoc88@gmail.com>
2021-12-02 11:40:22 +0900

    E-Mail認証対応

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth','verified']], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
